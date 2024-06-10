9026043ceba84e44beafb7e97e3f7fd1426d02aa
Nicholas Lai <nicholas_lai@hsdm.harvard.edu>
2024-06-10 17:23:30 +0000

    Incorporate Lawrence's changes for downloads

--- validFiles/download.R
+++ validFiles/download.R
@@ -3,7 +3,7 @@ library(shinyWidgets)
 library(tidyverse)
 
 # get files from the folder
-folder_path <- "dataFiles/ref_data/"
+folder_path <- "ref_data/"
 data_files <- list.files(folder_path)
 css <- ".shiny-notification {
           position:fixed;
@@ -21,7 +21,7 @@ css <- ".shiny-notification {
 
 "
 
-email <- reactiveValues(login = FALSE)
+# email <- reactiveValues(login = FALSE)
 
 bar <- function(placeholder) {
   output[[placeholder]] <- downloadHandler(
@@ -33,7 +33,28 @@ bar <- function(placeholder) {
   )
 }
 
-#folder_path <- "dataFiles/ref_data/"
+buildDownloadLink <- function(inputData) {
+  inputs <- lapply(inputData, function(args) {
+    do.call(downloadLink, args)
+  })
+  for (i in 1:length(full_data_files)) {
+    bar(full_data_files[i])
+  }
+  
+  inputs2 <- list()
+  for (i in 1:(3 * length(inputs)-1)) {
+    if (i %% 3 == 1) {
+      inputs2[[i]] <- img(src = "downloadLinkImg.png", Rd = FALSE, alt = "image", height = 11.5)
+    } else if (i %% 3 == 2) {
+      inputs2[[i]] <- inputs[[(i + 1) %/% 3]]
+    } else {
+      inputs2[[i]] <- HTML("<br>")
+    }
+  }
+  
+  do.call(div, inputs2)
+}
+
 folder_path <- "ref_data/"
 full_data_files <- list.files(folder_path)
 
@@ -44,63 +65,41 @@ for (i in 1:length(full_data_files)) {
                                 label = gsub('ref_data_', '',full_data_files[[i]]))
 }
 
-buildDownloadLink <- function(inputData) {
-  inputs <- lapply(inputData, function(args) {
-    do.call(downloadLink, args)
-  })
-  for (i in 1:length(full_data_files)) {
-    bar(full_data_files[i])
-  }
-  
-  inputs2 <- list()
-  for (i in 1:(2 * length(inputs)-1)) {
-    if (i %% 2 == 1) {
-      inputs2[[i]] <- inputs[[(i + 1) %/% 2]]
-    } else {
-      inputs2[[i]] <- HTML("<br>")
-    }
-  }
-  
-  do.call(div, inputs2)
-}
-
-
-
-isValidEmail <- function(x) {
-  grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>", as.character(x),
-        ignore.case=TRUE)
-}
+# isValidEmail <- function(x) {
+#   grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>", as.character(x),
+#         ignore.case=TRUE)
+# }
 
 output$record <- renderUI(
-  if (email$login) {
-    div(id = "downloads", style='display: flex; flex-direction: row; justify-content: space-between;',
-        buildDownloadLink(downloadLinkList))
-  }
-  else {
-    div(id = "login_panel",style = "width: 90%; padding: 20px;",
-        wellPanel(textInput("userName", "Please enter your email here to access the website: "),
-                  br(),actionButton("loginButton", "Log in")))
-  }
+  # if (email$login) {
+  div(id = "downloads", style='display: flex; flex-direction: row; justify-content: space-between;',
+      buildDownloadLink(downloadLinkList))
+  # }
+  # else {
+  #   div(id = "login_panel",style = "width: 90%; padding: 20px;",
+  #       wellPanel(textInput("userName", "Please enter your email here to access the website: "),
+  #                 br(),actionButton("loginButton", "Log in")))
+  # }
 )
 
-observeEvent(input$loginButton,{
-  if (isValidEmail(input$userName)) {
-    email$login = TRUE
-    name <- input$userName %>% as.list()
-    time <- c(as.character(Sys.time()))
-    zone <- c(as.character(Sys.timezone()))
-    data <- data.frame(name, time, zone)
-
-    # drive_auth(path = "client_secret.json")
-    gs4_auth(path = "client_secret.json")
-
-    sheet_append(ss = 'https://docs.google.com/spreadsheets/d/11yyHKU8VkvKeK8M79iEYk_joqtEV5hBDU_s1X1-UT3c/edit?usp=sharing',
-                 data = data,
-                 sheet = 1)
-  } else {
-    showNotification("Please enter a valid email address", type = "error")
-  }
-})
+# observeEvent(input$loginButton,{
+#   if (isValidEmail(input$userName)) {
+#     email$login = TRUE
+#     name <- input$userName %>% as.list()
+#     time <- c(as.character(Sys.time()))
+#     zone <- c(as.character(Sys.timezone()))
+#     data <- data.frame(name, time, zone)
+# 
+#     # drive_auth(path = "client_secret.json")
+#     gs4_auth(path = "client_secret.json")
+# 
+#     sheet_append(ss = 'https://docs.google.com/spreadsheets/d/11yyHKU8VkvKeK8M79iEYk_joqtEV5hBDU_s1X1-UT3c/edit?usp=sharing',
+#                  data = data,
+#                  sheet = 1)
+#   } else {
+#     showNotification("Please enter a valid email address", type = "error")
+#   }
+# })
 
 output$pageStub <- renderUI(fluidPage(
   useShinyjs(),
