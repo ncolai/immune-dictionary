2330e0d0322beb97de606d2d11dc4a2459384d80
Nicholas Lai <nicholas_lai@hsdm.harvard.edu>
2024-06-14 20:35:17 +0000

    Updated files to account for lig_seurat.Rda

--- validFiles/download.R
+++ validFiles/download.R
@@ -2,26 +2,27 @@ library(shinyjs)
 library(shinyWidgets)
 library(tidyverse)
 
-# get files from the folder
+# Define the paths
 folder_path <- "ref_data/"
+extra_file_path <- "dataFiles/lig_seurat.Rda"
+
+# List of data files
 data_files <- list.files(folder_path)
+full_data_files <- list.files(folder_path)
 css <- ".shiny-notification {
           position:fixed;
           top: calc(22%);
           left: calc(25%);
           width: 250px;
 }
-        
-        .item {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          text-align: center;
-        }
 
+.item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
 "
-
-# email <- reactiveValues(login = FALSE)
 
 bar <- function(placeholder) {
   output[[placeholder]] <- downloadHandler(
@@ -33,6 +34,15 @@ bar <- function(placeholder) {
   )
 }
 
+bar_extra <- function() {
+  output[["lig_seurat"]] <- downloadHandler(
+    filename = function() { "lig_seurat.Rda" },
+    content = function(fname) {
+      file.copy(extra_file_path, fname)
+    }
+  )
+}
+
 buildDownloadLink <- function(inputData) {
   inputs <- lapply(inputData, function(args) {
     do.call(downloadLink, args)
@@ -40,6 +50,8 @@ buildDownloadLink <- function(inputData) {
   for (i in 1:length(full_data_files)) {
     bar(full_data_files[i])
   }
+  
+  bar_extra()
   
   inputs2 <- list()
   for (i in 1:(3 * length(inputs)-1)) {
@@ -55,9 +67,7 @@ buildDownloadLink <- function(inputData) {
   do.call(div, inputs2)
 }
 
-folder_path <- "ref_data/"
-full_data_files <- list.files(folder_path)
-
+# Create download link list
 downloadLinkList <- list()
 
 for (i in 1:length(full_data_files)) {
@@ -65,45 +75,39 @@ for (i in 1:length(full_data_files)) {
                                 label = gsub('ref_data_', '',full_data_files[[i]]))
 }
 
-# isValidEmail <- function(x) {
-#   grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>", as.character(x),
-#         ignore.case=TRUE)
-# }
-
-output$record <- renderUI(
-  # if (email$login) {
-  div(id = "downloads", style='display: flex; flex-direction: row; justify-content: space-between;',
-      buildDownloadLink(downloadLinkList))
-  # }
-  # else {
-  #   div(id = "login_panel",style = "width: 90%; padding: 20px;",
-  #       wellPanel(textInput("userName", "Please enter your email here to access the website: "),
-  #                 br(),actionButton("loginButton", "Log in")))
-  # }
+downloadLinkList[[length(full_data_files) + 1]] <- list(
+  outputId = "lig_seurat", 
+  label = "lig_seurat.Rda"
 )
 
-# observeEvent(input$loginButton,{
-#   if (isValidEmail(input$userName)) {
-#     email$login = TRUE
-#     name <- input$userName %>% as.list()
-#     time <- c(as.character(Sys.time()))
-#     zone <- c(as.character(Sys.timezone()))
-#     data <- data.frame(name, time, zone)
-# 
-#     # drive_auth(path = "client_secret.json")
-#     gs4_auth(path = "client_secret.json")
-# 
-#     sheet_append(ss = 'https://docs.google.com/spreadsheets/d/11yyHKU8VkvKeK8M79iEYk_joqtEV5hBDU_s1X1-UT3c/edit?usp=sharing',
-#                  data = data,
-#                  sheet = 1)
-#   } else {
-#     showNotification("Please enter a valid email address", type = "error")
-#   }
-# })
+output$record <- renderUI(
+  div(id = "downloads", style='display: flex; flex-direction: row; justify-content: space-between;',
+      buildDownloadLink(downloadLinkList))
+)
 
 output$pageStub <- renderUI(fluidPage(
   useShinyjs(),
   tags$head(tags$style(css)),
   titlePanel("Download"),
-  uiOutput("record")
+  uiOutput("record"),
+  div(id = "note",
+      style = "margin-top: 20px; padding: 10px; border: 1px solid #ddd; background-color: #f9f9f9;",
+      h4("Note: How to Load RDS Files in R"),
+      p("How to load RDS files in R, don't worry we can help you! you can use the following code:"),
+      code('lig_seurat <- readRDS(paste0("path_to_your_file/"),celltype,".rds",sep = "")'),
+      p("Replace ", code('"path_to_your_file.rds"'), " with the actual path to your RDS file."),
+      h4("Note: How to Load Multiple RDS Files in R in one Seurat Object"),
+      code('lig_seurat <- vector(mode="list", length=15)'),br(),
+      code('lig_seurat[[celltype]] <- readRDS(paste0("path_to_your_file/",celltype, ".rds", sep = ""))'),
+      p("Replace ", code('"path_to_your_file.rds"'), " with the actual path to your RDS file. Replace ",code('"celltype"'),"with the celltype of interest"),
+      h4("lig_seurat.Rda File"),
+      p("The file ", code('lig_seurat.Rda'), " contains all cell types combined. This can be useful for comprehensive analysis across multiple cell types."),
+      h4("Additional code to see 'celltype', 'sample', 'rep', 'variable', 'value' columns"),
+      code('raw_data = lig_seurat[[input$cellType]]'),br(),
+      code('plot_df = cbind(t(as.matrix(raw_data@assays[["RNA"]]@data[input$InputGene,])),
+                    raw_data@meta.data)'),br(),
+      code('plot_df_melt = melt(plot_df, id.vars = names(raw_data@meta.data))'),
+      p("Replace ", code('"input$cellType"'), " with the celltype you are interested in. Replace ",code('"input$InputGene"'),"with the gene of interest")
+  )
 ))
+
