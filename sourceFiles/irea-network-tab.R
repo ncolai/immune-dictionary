c660809403acb856fe0cd4df45c2c005231a9570
Nicholas Lai <nicholaslai@Nicks-iMac.local>
2025-02-19 11:01:26 -0500

    Removed extra cytokine input section from IREA network

--- sourceFiles/irea-network-tab.R
+++ sourceFiles/irea-network-tab.R
@@ -37,149 +37,74 @@ genesC <- observeEvent(input$submit_cytokine_network, {
   suppressWarnings({
     start_time <- Sys.time()
     
-    if (input$network_type_cytokines == 'individual') { # & length(input$network_inputCytokines) > 0) {
-      if (!is.null(input$network_matrix_singlefile) | input$sample_network_matrix_singlefile) {
-        if (input$sample_network_matrix_singlefile) {
-          # use sample network tumor matrix data file
-          my_data <- read_excel('exampleFiles/network_test.xlsx')
-        } else {
-          # use uploaded file
-          table_data <- input$network_matrix_singlefile$datapath
-          my_data <- read_excel(table_data, .name_repair = ~ ifelse(nzchar(.x), .x, paste('Sample', seq_along(.x) - 1)))  # rename columns without names
-        }
-        
-        cat('calculating')
-        #data$my_data <- subset(my_data, "Cytokine" %in% input$network_inputCytokines)
-        data$input_profile_C <- make_matrix(select(my_data, -1), pull(my_data, 1)) # make matrix from input
-        
-        cells_to_analyze <- c() #vector of cells we want to analyze
-        if (input$sample_network_t_cell_cd4) {cells_to_analyze <- c(cells_to_analyze, "T_cell_CD4")}
-        if (input$sample_network_t_cell_cd8) {cells_to_analyze <- c(cells_to_analyze, "T_cell_CD8")}
-        if (input$sample_network_treg) {cells_to_analyze <- c(cells_to_analyze, "Treg")}
-        if (input$sample_network_nk_cell) {cells_to_analyze <- c(cells_to_analyze, "NK_cell")}
-        if (input$sample_network_pdc) {cells_to_analyze <- c(cells_to_analyze, "pDC")}
-        if (input$sample_network_cdc1) {cells_to_analyze <- c(cells_to_analyze, "cDC1")}
-        if (input$sample_network_cdc2) {cells_to_analyze <- c(cells_to_analyze, "cDC2")}
-        if (input$sample_network_migdc) {cells_to_analyze <- c(cells_to_analyze, "MigDC")}
-        if (input$sample_network_macrophage) {cells_to_analyze <- c(cells_to_analyze, "Macrophage")}
-        if (input$sample_network_neutrophil) {cells_to_analyze <- c(cells_to_analyze, "Neutrophil")}
-        
-        # Check if file format is correct
-        if (!isTRUE(all(is.numeric(data$input_profile_C) & -1000000 <= data$input_profile_C & data$input_profile_C <= 100000))) {
+    if (!is.null(input$network_matrix_singlefile) | input$sample_network_matrix_singlefile) {
+      if (input$sample_network_matrix_singlefile) {
+        # use sample network tumor matrix data file
+        my_data <- read_excel('exampleFiles/network_test.xlsx')
+      } else {
+        # use uploaded file
+        table_data <- input$network_matrix_singlefile$datapath
+        my_data <- read_excel(table_data, .name_repair = ~ ifelse(nzchar(.x), .x, paste('Sample', seq_along(.x) - 1)))  # rename columns without names
+      }
+      
+      cat('calculating')
+      data$input_profile_C <- make_matrix(select(my_data, -1), pull(my_data, 1)) # make matrix from input
+      
+      cells_to_analyze <- c() #vector of cells we want to analyze
+      if (input$sample_network_t_cell_cd4) {cells_to_analyze <- c(cells_to_analyze, "T_cell_CD4")}
+      if (input$sample_network_t_cell_cd8) {cells_to_analyze <- c(cells_to_analyze, "T_cell_CD8")}
+      if (input$sample_network_treg) {cells_to_analyze <- c(cells_to_analyze, "Treg")}
+      if (input$sample_network_nk_cell) {cells_to_analyze <- c(cells_to_analyze, "NK_cell")}
+      if (input$sample_network_pdc) {cells_to_analyze <- c(cells_to_analyze, "pDC")}
+      if (input$sample_network_cdc1) {cells_to_analyze <- c(cells_to_analyze, "cDC1")}
+      if (input$sample_network_cdc2) {cells_to_analyze <- c(cells_to_analyze, "cDC2")}
+      if (input$sample_network_migdc) {cells_to_analyze <- c(cells_to_analyze, "MigDC")}
+      if (input$sample_network_macrophage) {cells_to_analyze <- c(cells_to_analyze, "Macrophage")}
+      if (input$sample_network_neutrophil) {cells_to_analyze <- c(cells_to_analyze, "Neutrophil")}
+      
+      # Check if file format is correct
+      if (!isTRUE(all(is.numeric(data$input_profile_C) & -1000000 <= data$input_profile_C & data$input_profile_C <= 100000))) {
+        data$input_profile_C <- NULL
+        showNotification(HTML('Your data file format is incorrect.<br>Please make sure the first column is genes, and the following columns only contain numbers (in the range [-1 to 1]) pertaining to the sample.'), duration = NULL, type = 'error')
+      } else {
+        is_valid <- valid_genes(rownames(data$input_profile_C), species=tolower(input$speciesInputC))
+        if (identical(is_valid[1], 'invalid')) {
+          showNotification('Please submit valid genes in the first column.', duration = NULL, type = 'error')
           data$input_profile_C <- NULL
-          showNotification(HTML('Your data file format is incorrect.<br>Please make sure the first column is genes, and the following columns only contain numbers (in the range [-1 to 1]) pertaining to the sample.'), duration = NULL, type = 'error')
+          data$table_tabC = NULL
+          data$irea_plot_C = NULL
         } else {
-          is_valid <- valid_genes(rownames(data$input_profile_C), species=tolower(input$speciesInputC))
-          if (identical(is_valid[1], 'invalid')) {
-            showNotification('Please submit valid genes in the first column.', duration = NULL, type = 'error')
-            data$input_profile_C <- NULL
+          if (!identical(is_valid[1], 'valid')) {
+            # handle invalid genes message if needed
+          }
+          cat('Calculating Cytokine Network Analysis...\n')
+          tryCatch({
+            cat("Input Cytokine Network\n")
+            df_irea_all <- IreaAll(data$input_profile_C, species = input$speciesInputC, threshold_receptor = 0.05, threshold_ligand = 0.05, celltypes = cells_to_analyze)
+            df_irea_network <- IreaNetwork(df_irea_all, require_receptor_expression = TRUE)
+            df_irea_network_pos <- subset(df_irea_network, ES > 0)
+            if (nrow(df_irea_network_pos) == 0) {
+              stop("No significant connection data found for selected combination, please try again.")
+            }
+            data$table_tabC <- df_irea_network_pos
+            cat("Got data table\n")
+          },
+          error = function(e) {
+            showNotification(paste0("Error: ", e), duration = NULL, type = 'error')
+            data$input_profile_C = NULL
             data$table_tabC = NULL
             data$irea_plot_C = NULL
-          } else {
-            if (!identical(is_valid[1], 'valid')) {
-              # handle invalid genes message if needed
-            }
-            cat('Calculating Cytokine Network Analysis...\n')
-            tryCatch({
-              cat("Input Cytokine Network\n")
-              #df_irea_all <- IreaAll(data$input_profile_C, species = "mouse", threshold_receptor = 0.05, threshold_ligand = 0.05, celltypes = cells_to_analyze)
-              df_irea_all <- IreaAll(data$input_profile_C, species = input$speciesInputC, threshold_receptor = 0.05, threshold_ligand = 0.05, celltypes = cells_to_analyze)
-              df_irea_network <- IreaNetwork(df_irea_all, require_receptor_expression = TRUE)
-              df_irea_network_pos <- subset(df_irea_network, ES > 0)
-              if (nrow(df_irea_network_pos) == 0) {
-                stop("No significant connection data found for selected combination, please try again.")
-              }
-              data$table_tabC <- df_irea_network_pos
-              cat("Got data table\n")
-            },
-            error = function(e) {
-              showNotification(paste0("Error: ", e), duration = NULL, type = 'error')
-              data$input_profile_C = NULL
-              data$table_tabC = NULL
-              data$irea_plot_C = NULL
-            })
-          }
+          })
         }
-      } else {
-        showNotification('Please upload gene matrix file', type = 'error')
       }
-      runjs("$('.busy').hide();")
-      showElement("plot_C")
-      showElement("download_C")
-      showElement("table_C")
-      showElement("download_table_C")
-    } else if (input$network_type_cytokines == 'inp' & length(input$network_inputCytokines) <= 0) {
-      showNotification('Please enter cytokines.', type = 'error')
-    } else if (input$network_type_cytokines == 'all') {
-      if (!is.null(input$network_matrix_singlefile) | input$sample_network_matrix_singlefile) {
-        if (input$sample_network_matrix_singlefile) {
-          # use sample network tumor matrix data file
-          my_data <- read_excel('exampleFiles/network_test.xlsx')
-        } else {
-          # use uploaded file
-          table_data <- input$network_matrix_singlefile$datapath
-          my_data <- read_excel(table_data, .name_repair = ~ ifelse(nzchar(.x), .x, paste('Sample', seq_along(.x) - 1)))  # rename columns without names
-        }
-        
-        cat('calculating')
-        data$input_profile_C <- make_matrix(select(my_data, -1), pull(my_data, 1)) # make matrix from input
-        
-        cells_to_analyze <- c() #vector of cells we want to analyze
-        if (input$sample_network_t_cell_cd4) {cells_to_analyze <- c(cells_to_analyze, "T_cell_CD4")}
-        if (input$sample_network_t_cell_cd8) {cells_to_analyze <- c(cells_to_analyze, "T_cell_CD8")}
-        if (input$sample_network_treg) {cells_to_analyze <- c(cells_to_analyze, "Treg")}
-        if (input$sample_network_nk_cell) {cells_to_analyze <- c(cells_to_analyze, "NK_cell")}
-        if (input$sample_network_pdc) {cells_to_analyze <- c(cells_to_analyze, "pDC")}
-        if (input$sample_network_cdc1) {cells_to_analyze <- c(cells_to_analyze, "cDC1")}
-        if (input$sample_network_cdc2) {cells_to_analyze <- c(cells_to_analyze, "cDC2")}
-        if (input$sample_network_migdc) {cells_to_analyze <- c(cells_to_analyze, "MigDC")}
-        if (input$sample_network_macrophage) {cells_to_analyze <- c(cells_to_analyze, "Macrophage")}
-        if (input$sample_network_neutrophil) {cells_to_analyze <- c(cells_to_analyze, "Neutrophil")}
-        
-        # Check if file format is correct
-        if (!isTRUE(all(is.numeric(data$input_profile_C) & -1000000 <= data$input_profile_C & data$input_profile_C <= 100000))) {
-          data$input_profile_C <- NULL
-          showNotification(HTML('Your data file format is incorrect.<br>Please make sure the first column is genes, and the following columns only contain numbers (in the range [-1 to 1]) pertaining to the sample.'), duration = NULL, type = 'error')
-        } else {
-          is_valid <- valid_genes(rownames(data$input_profile_C), species=tolower(input$speciesInputC))
-          if (identical(is_valid[1], 'invalid')) {
-            showNotification('Please submit valid genes in the first column.', duration = NULL, type = 'error')
-            data$input_profile_C <- NULL
-            data$table_tabC = NULL
-            data$irea_plot_C = NULL
-          } else {
-            if (!identical(is_valid[1], 'valid')) {
-              # handle invalid genes message if needed
-            }
-            cat('Calculating Cytokine Network Analysis...\n')
-            tryCatch({
-              cat("Input Cytokine Network\n")
-              df_irea_all <- IreaAll(data$input_profile_C, species = input$speciesInputC, threshold_receptor = 0.05, threshold_ligand = 0.05, celltypes = cells_to_analyze)
-              df_irea_network <- IreaNetwork(df_irea_all, require_receptor_expression = TRUE)
-              df_irea_network_pos <- subset(df_irea_network, ES > 0)
-              if (nrow(df_irea_network_pos) == 0) {
-                stop("No significant connection data found for selected combination, please try again.")
-              }
-              data$table_tabC <- df_irea_network_pos
-              cat("Got data table\n")
-            },
-            error = function(e) {
-              showNotification(paste0("Error: ", e), duration = NULL, type = 'error')
-              data$input_profile_C = NULL
-              data$table_tabC = NULL
-              data$irea_plot_C = NULL
-            })
-          }
-        }
-      } else {
-        showNotification('Please upload gene matrix file', type = 'error')
-      }
-      runjs("$('.busy').hide();")
-      showElement("plot_C")
-      showElement("download_C")
-      showElement("table_C")
-      showElement("download_table_C")
+    } else {
+      showNotification('Please upload gene matrix file', type = 'error')
     }
+    runjs("$('.busy').hide();")
+    showElement("plot_C")
+    showElement("download_C")
+    showElement("table_C")
+    showElement("download_table_C")
     
     end_time <- Sys.time()
     elapsed_time <- end_time - start_time
@@ -195,13 +120,7 @@ output$table_C <- renderDataTable({
 
 plotInput_C <- function(){
   req(data$table_tabC)
-    if (input$network_type_cytokines == "all") {
-      data$irea_plot_C <- IreaNetworkCircosAll(data$table_tabC)
-    }
-    else if (input$network_type_cytokines == "individual") {
-      data$irea_plot_C <- IreaNetworkCircosIndividual(data$table_tabC, n_cols = 4)#, input$network_inputCytokines) 
-    }
-  # }
+  data$irea_plot_C <- IreaNetworkCircosAll(data$table_tabC)
   
   data$irea_plot_C
 }
